6c954d5a8ce1a4981e0aed2076538b40d2cbcdc9
dhrumit <dhrumitbarot1@gmail.com>
2023-01-26 01:45:04 -0600

    add new function for make diffrence with if

--- script.js
+++ script.js
@@ -5,3 +5,7 @@ let html = "";
 
 for (let i = 0; i < data.pokemon.length; i++) { // html +=`<p>${data.pokemon[i].name}: ${data.pokemon[i].num}</p>`;
     let next_evolution = 'none'}
+
+    if (data.pokemon[i].next_evolution) {
+        next_evolution = data.pokemon[i].next_evolution[0].name + data.pokemon[i].next_evolution[0].num;
+      }
